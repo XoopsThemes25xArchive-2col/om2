429880c1761fb19abcbfdb85c03a8c2bbf9e749e
mambax7 <mambax7@gmail.com>
2015-06-06 05:33:51 -0400

    Original files

---
+++ om2/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "omn2";
+	$txc_themeinfo["author"] = "omnetwork";
+	$txc_themeinfo["author_url"] = "http://www.omnetwork.net";
+	$txc_themeinfo["detail_url"] = "http://www.omnetwork.net/cms/modules/rmdp/down.php?id=70";
+	$txc_themeinfo["download_url"] = "http://www.omnetwork.net/cms/modules/rmdp/down.php?id=70";
+?>
